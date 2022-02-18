a50a0e3cd668d32f9a182dfa6cdc24452ae6c64f
Valentine Bodart <valentine.bodart@gmail.com>
2022-02-18 11:39:21 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,10 @@ movies.each do |movie|
   Movie.create(title: movie['original_title'], overview: movie['overview'],
                poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}", rating: movie['vote_average'])
 end
+
+Movie.create(title: 'Taken', overview: "Que peut-on imaginer de pire pour un père que d'assister
+            impuissant à l'enlèvement de sa fille via un téléphone portable ? C'est le cauchemar
+            vécu par Bryan, ancien agent des services secrets américains, qui n'a que quelques
+            heures pour arracher Kim des mains d'un redoutable gang spécialisé dans la traite
+            des femmes. Premier problème à résoudre : il est à Los Angeles, elle vient de se
+            faire enlever à Paris.", poster_url: 'https://image.tmdb.org/t/p/w500/t/p/w300_and_h450_bestv2/4h9b8E2C9FdIVKbQRDMwZzpSIjL.jpg', rating: '7.4')
